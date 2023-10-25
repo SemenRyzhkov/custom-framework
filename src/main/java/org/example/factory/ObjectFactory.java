0f36c72d7f19dc943acedceca019c903f87acfc8
sryzhkov <sryzhkov@neoflex.ru>
2023-10-25 14:56:00 +0300

    solution 8, task 9: add proxy pattern for changing methods behavior

--- src/main/java/org/example/factory/ObjectFactory.java
+++ src/main/java/org/example/factory/ObjectFactory.java
@@ -2,6 +2,7 @@ package org.example.factory;
 
 import lombok.Setter;
 import lombok.SneakyThrows;
+import org.example.annotation.PostConstruct;
 import org.example.config.Config;
 import org.example.config.JavaConfig;
 import org.example.config.configurator.ObjectConfigurator;
@@ -9,6 +10,8 @@ import org.example.context.ApplicationContext;
 import org.example.service.preparator.Preparator;
 import org.example.service.preparator.PreparatorImpl;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,8 +33,33 @@ public class ObjectFactory {
 
     @SneakyThrows
     public <T> T createObject(Class<T> implClass) {
-        T t = implClass.getDeclaredConstructor().newInstance();
+        T t = create(implClass);
+        configure(t);
+        //где должны находиться инвокерв для инит методов?
+        //ответ: оставим фабрике, т.к ее респонсибилити настраивать метод(вызывая конструктор)
+        //инит метод - это по сути вторичный конструктор
+        //если же выносить в один из конфигураторов(как в спринге), то нужно будет, чтобы он вызывался последним -
+        //приходим к ситуации, когда становится важным ордеринг, а мы этого не хотим
+        invokeInit(implClass, t);
+        //todo добавить возможность клиентам менять поведение методов
+        //add proxy pattern
+        return t;
+    }
+
+    private <T> void invokeInit(Class<T> implClass, T t) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : implClass.getMethods()) {
+            if (method.isAnnotationPresent(PostConstruct.class)) {
+                method.invoke(t);
+            }
+        }
+    }
+
+    private <T> void configure(T t) {
         configurators.forEach(objectConfigurator -> objectConfigurator.configure(t, context));
+    }
+
+    private <T> T create(Class<T> implClass) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        T t = implClass.getDeclaredConstructor().newInstance();
         return t;
     }
 }
--- src/main/java/org/example/service/preparator/PreparatorImpl.java
+++ src/main/java/org/example/service/preparator/PreparatorImpl.java
@@ -1,13 +1,20 @@
 package org.example.service.preparator;
 
 import org.example.annotation.InjectByType;
+import org.example.annotation.PostConstruct;
 import org.example.annotation.Singleton;
 import org.example.service.announcer.checker.Checker;
-
 @Singleton
 public class PreparatorImpl implements Preparator {
     @InjectByType
     Checker checker;
+
+    @PostConstruct
+    public void init() {
+        //problem нельзя настраивать объект, пока он не создался -> NPE
+        //todo для настройки объекта реализовать second face constructor
+        System.out.println(checker.getClass());
+    }
 
     @Override
     public void prepareForBeginWork() {
